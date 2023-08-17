ad4e40398121ea6fc97e28db1009ad1a2abe9975
Tommie Nygren <tommie.nygren@gmail.com>
2023-08-17 14:52:37 +0200

    Many more things work

--- src/systems.rs
+++ src/systems.rs
@@ -1,4 +1,4 @@
-use crate::components::{CameraFollow, InWater, Player, GameCam};
+use crate::components::{CameraFollow, Player, GameCam};
 use crate::{Layer, METERS_PER_PIXEL, PIXELS_PER_METER};
 use bevy::asset::{AssetServer};
 use bevy::input::keyboard::KeyboardInput;
@@ -10,53 +10,54 @@ use bevy::prelude::{
 use bevy::render::camera::ScalingMode;
 use bevy_xpbd_2d::components::{
     Collider, CollisionLayers, ExternalForce, Position, RigidBody};
+use bevy_xpbd_2d::prelude::{Friction, Restitution};
 
 pub fn spawn_player(
     mut commands: Commands,
     asset_server: Res<AssetServer>) {
-    let x = 0.0;
-    let y = 0.0;
-        commands
-            .spawn((
-                CameraFollow {},
-                InWater {},
-                SpriteBundle {
-                    transform: Transform::from_xyz(
-                        x * PIXELS_PER_METER,
-                        y * PIXELS_PER_METER,
-                        1.0,
-                    )
+    commands
+        .spawn((
+            CameraFollow {},
+            SpriteBundle {
+                transform: Transform::from_xyz(
+                    0.0,
+                    0.0,
+                    1.0,
+                )
                     .with_scale(Vec3::new(
-                        10.0,
-                        10.0,
+                        METERS_PER_PIXEL,
+                        METERS_PER_PIXEL,
                         1.0,
                     )),
-                    texture: asset_server.load("sprites/person.png"),
-                    ..default()
-                },
-                Player {},
-                RigidBody::Dynamic,
-                Position::from(Vec2 {
-                    x: x * PIXELS_PER_METER,
-                    y: y * PIXELS_PER_METER,
-                }),
-                ExternalForce::default().with_persistence(false),
-                Collider::cuboid(16.0 * METERS_PER_PIXEL, 8.0 * METERS_PER_PIXEL),
-                CollisionLayers::new([Layer::Player], [Layer::Walls, Layer::Water]),
-            ));
+                texture: asset_server.load("sprites/person.png"),
+                ..default()
+            },
+            Player {},
+            RigidBody::Dynamic,
+            Friction::default(),
+            Restitution::default(),
+            Position::from(Vec2 {
+                x: 0.0,
+                y: 0.0,
+            }),
+            ExternalForce::default().with_persistence(false),
+            Collider::cuboid(16.0 * METERS_PER_PIXEL, 8.0 * METERS_PER_PIXEL),
+            CollisionLayers::new([Layer::Player], [Layer::Walls, Layer::Water]),
+        ));
 }
 
 pub fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera2dBundle {
             projection: OrthographicProjection {
-                scale: METERS_PER_PIXEL * 10.0,
+                scale: METERS_PER_PIXEL * 2.0,
                 near: 0.0,
                 far: 1000.0,
                 viewport_origin: Vec2::new(0.5, 0.5),
                 scaling_mode: ScalingMode::WindowSize(PIXELS_PER_METER),
                 area: Rect::new(-1.0, -1.0, 1.0, 1.0),
             },
+            transform: Transform::from_xyz(0.0, 0.0, 999.0),
             ..default()
         },
         GameCam {},
@@ -75,7 +76,7 @@ pub fn camera_follow(
         z: camera_transform.translation.z,
     };
 
-    camera_transform.translation = camera_transform.translation.lerp(target, 0.1);
+    camera_transform.translation = camera_transform.translation.lerp(target, 0.5);
 }
 
 pub fn keyboard_input(
@@ -85,28 +86,42 @@ pub fn keyboard_input(
     use bevy::input::ButtonState;
     use bevy::math::vec2;
     use bevy::prelude::KeyCode;
+    let mut dir_force = vec2(0.0, 0.0);
     if let Ok(mut external_force) = query.get_single_mut() {
         for ev in key_evr.iter() {
             match ev.state {
                 ButtonState::Pressed => match ev.key_code {
                     Some(KeyCode::A) => {
-                        external_force.apply_force(vec2(-5.0, 0.0));
+                        dir_force.x -= 5.0;
                     }
                     Some(KeyCode::D) => {
-                        external_force.apply_force(vec2(5.0, 0.0));
+                        dir_force.x += 5.0;
                     }
                     Some(KeyCode::W) => {
-                        external_force.apply_force(vec2(0.0, 5.0));
+                        dir_force.y += 5.0;
                     }
                     Some(KeyCode::S) => {
-                        external_force.apply_force(vec2(0.0, -5.0));
+                        dir_force.y -= 5.0;
                     }
                     _ => {}
                 },
-                ButtonState::Released => {
-                    println!("Key release: {:?} ({})", ev.key_code, ev.scan_code);
+                ButtonState::Released => match ev.key_code {
+                    Some(KeyCode::A) => {
+                        dir_force.x = 0.0;
+                    }
+                    Some(KeyCode::D) => {
+                        dir_force.x = 0.0;
+                    }
+                    Some(KeyCode::W) => {
+                        dir_force.y = 0.0;
+                    }
+                    Some(KeyCode::S) => {
+                        dir_force.y = 0.0;
+                    }
+                    _ => {}
                 }
             }
         }
+        external_force.apply_force(dir_force);
     }
 }
