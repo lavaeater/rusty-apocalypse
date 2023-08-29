9806480922743cdb94299cb2ce83082466022373
Tommie Nygren <tommie.nygren@gmail.com>
2023-08-29 11:06:15 +0200

    Started implementation of my facts-of-the-world concept

--- src/resources/facts_of_the_world.rs
+++ src/resources/facts_of_the_world.rs
@@ -1,6 +1,19 @@
+use bevy::utils::HashMap;
+
 pub enum Fact {
     StringFact(String),
     IntFact(i32),
     FloatFact(f32),
     BoolFact(bool),
+    ListFact(Vec<Fact>),
+}
+
+pub struct FactsOfTheWorld {
+    facts: HashMap<String, Fact>,
+}
+
+impl FactsOfTheWorld {
+    pub fn get_fact(key: &str) -> Option<&Fact> {
+        None
+    }
 }
